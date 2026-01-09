8042bf04264d721309dca3c56c9ca60ebf1a5586
Sahed Saad <sahedsaad2006@gmail.com>
2026-01-08 23:57:19 -0500

    Replace utc+8 conversion fucntion to EST time

--- Util_Functions.py
+++ Util_Functions.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, timezone
-
+from zoneinfo import ZoneInfo
 
 def wind_degree_to_direction(str_wind_degree):
     """
@@ -39,36 +39,15 @@ def wind_degree_to_direction(str_wind_degree):
     index = int((wind_degree + 11.25) // 22.5) % 16
     return directions[index]
 
-
-def unix_timestamp_to_localtime(str_unix_timestamp, str_timezone_offset_seconds):
-    """
-    Convert unix timestamp to localtime (for sunrise and sunset).
-
-    Parameters:
-    str_unix_timestamp (str): Unix timestamp (e.g., "1717715516")
-    str_timezone_offset_seconds (str): timezone offset in second (e.g., "28800" represents UTC+8)
-
-    Returns:
-    str: local_time (e.g., "2024-06-07 07:11:56")
-         Or message "API sunset/sunrise data format error!"
-         Or message "API timezone data format error!"
-    """
-    # Convert strings to integers
+def unix_timestamp_to_nyc_time(str_unix_timestamp):
     try:
         unix_timestamp = int(str_unix_timestamp)
     except ValueError:
-        return "API sunset/sunrise data format error!"
+        return "API format error!"
 
-    try:
-        timezone_offset_seconds = int(str_timezone_offset_seconds)
-    except ValueError:
-        return "API timezone data format error!"
-
-    # Convert Unix timestamp to UTC datetime
-    utc_time = datetime.fromtimestamp(unix_timestamp, tz=timezone.utc)
-
-    # Apply timezone offset
-    local_time = utc_time + timedelta(seconds=timezone_offset_seconds)
+    # converts the timestamp directly to NYC time, EST
+    nyc_tz = ZoneInfo("America/New_York")
+    local_time = datetime.fromtimestamp(unix_timestamp, tz=nyc_tz)
 
     return local_time.strftime("%Y-%m-%d %H:%M:%S")
 
